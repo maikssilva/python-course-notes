35c0f1ac6a3ba0b7a53fcd8d579562de1e991194
maikssilva <maikstrader@gmail.com>
2025-03-23 15:17:01 -0300

    Curso Professor Guanabara de Python

---
+++ Prof Guanabara - Python/Exercicios/ex06.py
@@ -0,0 +1,3 @@
+n = int(input('Digite um número: '))
+print(f'Analisando o número {n}, o dobro desse número é {n*2}, o triplo é {n*3} e a raiz quadrada é {n**(1/2):2f}')
+# Compare this snippet from Exercicios/ex07.py:
