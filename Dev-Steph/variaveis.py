c7a7c804ed1e1fe13e24ad3d6279d70a8b0fae35
maikssilva <maikstrader@gmail.com>
2025-03-29 23:17:33 -0300

    início do curso de python da academia dev

---
+++ Dev-Steph/variaveis.py
@@ -0,0 +1,14 @@
+# Variáveis e tipos de dados "básicos"
+
+# Variáveis são espaços na memória que guardam valores
+
+# Tipos de dados básicos: int, float, str, bool
+
+<nome da var> = <valor>  # Atribuição de valor a uma variável
+
+nome = "Maik" # Variável do tipo string (str), (texto), sempre entre aspas, (simples ou duplas)
+idade = 32 # Variável do tipo inteiro (int), (recebe número sem casas decimais), não precisa de aspas
+altura = 1.92 # Variável do tipo float, (recebe número com casas decimais), não precisa de aspas
+dev = True # Variável do tipo booleano (bool), (recebe True ou False), não precisa de aspas
+
+print(f"Olá, {nome}! Você tem {idade} anos e {altura}m de altura. Você é dev? {dev}. ")
