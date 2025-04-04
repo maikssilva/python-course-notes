89f486b4455626c6f98498d066cae9ce95714c57
maikssilva <maikstrader@gmail.com>
2025-04-03 23:29:20 -0300

    exercicios da aula 9

--- Professor-Guanabara-Python/Exercicios m03/ex21.py
+++ Professor-Guanabara-Python/Exercicios m03/ex21.py
@@ -1,10 +1,11 @@
 # Faça um programa em Python que abra e reproduza o áudio de um arquivo MP3
 
-import pygame
+# import pygame
 
-pygame.mixer.init()
+# pygame.mixer.init()
 
-pygame.mixer.music.load("audio.mp3")
-pygame.mixer.music.play()
+# pygame.mixer.music.load("audio.mp3")
+# pygame.mixer.music.play()
 
-input("Pressione Enter para parar a música...")
+# input("Pressione Enter para parar a música...")
+
---
+++ Professor-Guanabara-Python/Exercicios m03/ex22.py
@@ -0,0 +1,19 @@
+#Analisador de textos
+
+#Crie um programa que leia o nome completo de uma pessoa e mostre:
+# - O nome com todas as letras maiúsculas e minúsculas.
+# - Quantas letras ao todo (sem considerar espaços).
+# - Quantas letras tem o primeiro nome.
+
+nome = input("Digite seu nome completo: ")
+print(f'Seu nome é: {nome}')
+
+print(f'Seu nome com todas as letras maiúsculas é: {nome.upper()}')
+
+print(f'Seu nome com todas as letras minúsculas é: {nome.lower()}')
+
+print(f'Seu nome tem {len(nome) - nome.count(" ")} letras.')
+
+print(f'Seu primeiro nome é {nome.split()[0]} e ele tem {len(nome.split()[0])} letras.')
+
+
