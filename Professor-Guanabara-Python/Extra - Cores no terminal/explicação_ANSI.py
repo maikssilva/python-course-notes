54191f8f3f8d187c758efc99930f92cf228faf67
maikssilva <maikstrader@gmail.com>
2025-04-05 20:24:56 -0300

    cores

---
+++ Professor-Guanabara-Python/Extra - Cores no terminal/explicação_ANSI.py
@@ -0,0 +1,13 @@
+#ANSI - escape sequence
+# O código ANSI é uma sequência de caracteres que é interpretada pelo terminal para aplicar formatação ao texto, como cores e estilos.
+# O código inicia com \ e depois o código ANSI, seguido de um "m" para indicar o final da sequência.
+# Exemplo: \033[31m para vermelho, \033[0m para resetar a formatação.
+
+# \033[3valores - style - text - back e finaliza com m
+
+# códigos para estyle - 0 - 1(negrito) - 4(sublinhado) - 7(inverter cores)
+
+# códigos para text - 30(cinza) - 31(vermelho) - 32(verde) - 33(amarelo) - 34(azul) - 35(magenta) - 36(ciano) - 37(branco)
+
+# códigos para back - 40(branco) - 41(vermelho) - 42(verde) - 43(amarelo) - 44(azul) - 45(magenta) - 46(ciano) - 47(cinza)
+
