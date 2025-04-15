a1ebe3c6961031cea7860f17c59bda5d00eba5ae
maikssilva <maikstrader@gmail.com>
2025-04-14 21:55:21 -0300

    laços de repetição

---
+++ Professor-Guanabara-Python-M2/aulas_12_13_14/aulas13.py
@@ -0,0 +1,6 @@
+# Estruturas de controle 
+# Laços de repetição são utilizados para repetir um bloco de código várias vezes.
+# Existem dois tipos de laços de repetição em Python:
+# 1. Laços de repetição com contagem definida (for)
+# 2. Laços de repetição sem contagem definida (while)
+
