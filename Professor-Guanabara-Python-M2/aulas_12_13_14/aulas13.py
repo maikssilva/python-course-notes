7a2088bb04f83dbed7246e52533af214ae90707c
maikssilva <maikstrader@gmail.com>
2025-04-14 22:30:45 -0300

    Update aulas13.py

--- Professor-Guanabara-Python-M2/aulas_12_13_14/aulas13.py
+++ Professor-Guanabara-Python-M2/aulas_12_13_14/aulas13.py
@@ -4,3 +4,20 @@
 # 1. Laços de repetição com contagem definida (for)
 # 2. Laços de repetição sem contagem definida (while)
 
+# Prática   
+
+# for c in range(0,7, 2):
+#     print(c)
+# print('FIM')    
+
+# n = int(input('Digite um número: '))
+# for c in range(0, n+1):
+#     print(c)
+# print('FIM')     
+
+i = int(input('Início:  '))
+f = int(input('FIM: ')) 
+p = int(input('Passo:   '))
+for c in range(i, f+1, p):
+    print(c)
+print('FIM')
